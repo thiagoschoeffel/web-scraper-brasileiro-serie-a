d75833cf65eca8a0afed8868f0e7cffe5761f080
Thiago Schoeffel <thiago@7hs.com.br>
2021-02-18 16:06:36 -0300

    Start application repository

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+
+const server = express();
+
+server.get('/brasileiro-serie-a', async (request, response) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://globoesporte.globo.com/futebol/brasileirao-serie-a/');
+
+    const teamList = await page.evaluate(() => {
+        const nodeList = document.querySelectorAll('.tabela__equipes .classificacao__tabela--linha');
+        const teamArray = [...nodeList];
+        const teamList = teamArray.map((team) => ({
+            name: team.querySelector('.classificacao__equipes--nome').innerHTML,
+            position: team.querySelector('.classificacao__equipes--posicao').innerHTML
+        }));
+
+        return teamList
+    });
+
+    await browser.close();
+
+    response.send(teamList);
+});
+
+const port = 3000;
+server.listen(port, () => {
+    console.log(`
+        Servidor iniciado com sucesso!
+        acesse http://localhost:${port}
+    `);
+});
